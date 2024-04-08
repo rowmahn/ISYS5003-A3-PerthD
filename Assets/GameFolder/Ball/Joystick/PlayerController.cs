8b7332c5bb230dcd38497e9ce354132a1acce935
Roman Dulal <romandulal@Rowmahns-MacBook-Air.local>
2024-04-08 11:19:15 +0800

    Added coin collection audio

--- Assets/GameFolder/Ball/Joystick/PlayerController.cs
+++ Assets/GameFolder/Ball/Joystick/PlayerController.cs
@@ -1,52 +1,52 @@
-using UnityEngine;
+    using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
-public class PlayerController : MonoBehaviour
-{
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private FixedJoystick _joystick;
-
-    
-    [SerializeField]private float Speed = 5;
-    
-
-    int score = 0;
-    public int winScore;
-    public GameObject winText;
-
-
-    private void FixedUpdate()
+    [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+    public class PlayerController : MonoBehaviour
     {
-     
-    
-        //Step #2
-        //Change Input.GetAxis (or the input that you using) to Joystick.Vertical or Joystick.Horizontal
-        float v = _joystick.Vertical; //get the vertical value of joystick
-        float h = _joystick.Horizontal;//get the horizontal value of joystick
-    
-        //in case you using keys instead of axis (due keys are bool and not float) you can do this:
-        //bool isKeyPressed = (Joystick.Horizontal > 0) ? true : false;
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private FixedJoystick _joystick;
 
-        //ready!, you not need more.
-        Vector3 translate = (new Vector3(h, 0, v) * Time.deltaTime)*Speed;
-        transform.Translate(translate);
     
-    }
+        [SerializeField]private float Speed = 5;
+    
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Coin")
+        int score = 0;
+        public int winScore;
+        public GameObject winText;
+        public AudioSource audioPlayer;
+
+
+        private void FixedUpdate()
         {
-            Debug.Log("Coin collected!"); // Check if coin is detected
-                                          //Deactivate the coin when collided
-            other.gameObject.SetActive(false);
-            score++;
+     
+            float v = _joystick.Vertical; //get the vertical value of joystick
+            float h = _joystick.Horizontal;//get the horizontal value of joystick
+    
+       
+            Vector3 translate = (new Vector3(h, 0, v) * Time.deltaTime)*Speed;
+            transform.Translate(translate);
+    
+        }
 
-            //You win text display
-            if (score >= winScore)
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Coin")
             {
-                winText.SetActive(true);
+                // Check if coin is detected
+                Debug.Log("Coin collected!"); 
+
+                //Play sound when coin is collected
+                audioPlayer.Play();
+
+                //Deactivate the coin when collided
+                other.gameObject.SetActive(false);
+                score++;
+
+                //You win text display
+                if (score >= winScore)
+                {
+                    winText.SetActive(true);
+                }
             }
         }
     }
-}
