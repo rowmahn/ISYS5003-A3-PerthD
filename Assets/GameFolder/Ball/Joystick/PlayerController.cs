751998ae72fd902d559ff5e8553ba48d434e3df0
Roman Dulal <romandulal@Rowmahns-MacBook-Air.local>
2024-04-09 11:46:30 +0800

    Added game over text

--- Assets/GameFolder/Ball/Joystick/PlayerController.cs
+++ Assets/GameFolder/Ball/Joystick/PlayerController.cs
@@ -1,58 +1,83 @@
-    using UnityEngine;
+        using UnityEngine;
 
-    [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
-    public class PlayerController : MonoBehaviour
-    {
-        [SerializeField] private Rigidbody _rigidbody;
-        [SerializeField] private FixedJoystick _joystick;
+        [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+        public class PlayerController : MonoBehaviour
+        {
+            [SerializeField] private Rigidbody _rigidbody;
+            [SerializeField] private FixedJoystick _joystick;
 
     
-        [SerializeField]private float Speed = 5;
+            [SerializeField]private float Speed = 5;
     
 
-        int score = 0;
-        public int winScore;
-        public GameObject winText;
-        public AudioSource audioPlayer;
-        public AudioClip coinCollectSound, winSound;
-     
-        
+            int score = 0;
+            public int winScore;
+            public GameObject winText;
+            public AudioSource audioPlayer;
+            public AudioClip coinCollectSound, winSound;
+
+        public float fallThreshold = -2f; 
+        private bool isGameOver = false;
+        public GameObject gameOverText;
+
+
 
 
         private void FixedUpdate()
-        {
+            {
      
-            float v = _joystick.Vertical; //get the vertical value of joystick
-            float h = _joystick.Horizontal;//get the horizontal value of joystick
+                float v = _joystick.Vertical; //get the vertical value of joystick
+                float h = _joystick.Horizontal;//get the horizontal value of joystick
     
        
-            Vector3 translate = (new Vector3(h, 0, v) * Time.deltaTime)*Speed;
-            transform.Translate(translate);
+                Vector3 translate = (new Vector3(h, 0, v) * Time.deltaTime)*Speed;
+                transform.Translate(translate);
     
-        }
+            }
 
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.tag == "Coin")
+            private void OnTriggerEnter(Collider other)
             {
-                // Check if coin is detected
-                Debug.Log("Coin collected!");
-
-            //Play sound when coin is collected
-            audioPlayer.clip = coinCollectSound;
-            audioPlayer.Play();
-
-                //Deactivate the coin when collided
-                other.gameObject.SetActive(false);
-                score++;
-
-                //You win text display
-                if (score >= winScore)
+                if (other.gameObject.tag == "Coin")
                 {
-                    audioPlayer.clip = winSound;
-                    audioPlayer.Play();
-                    winText.SetActive(true);
+                    // Check if coin is detected
+                    Debug.Log("Coin collected!");
+
+                //Play sound when coin is collected
+                audioPlayer.clip = coinCollectSound;
+                audioPlayer.Play();
+
+                    //Deactivate the coin when collided
+                    other.gameObject.SetActive(false);
+                    score++;
+
+                    //You win text display
+                    if (score >= winScore)
+                    {
+                        audioPlayer.clip = winSound;
+                        audioPlayer.Play();
+                        winText.SetActive(true);
+                    }
                 }
             }
+        void Update()
+        {
+            // Check if the game is already over to avoid redundant checks
+            if (!isGameOver)
+            {
+                // Check if the ball falls below the threshold
+                if (transform.position.y < fallThreshold)
+                {
+            
+                    GameOver();
+                }
+            }
+        }
+
+        void GameOver()
+        {
+            // Set the game over flag to true to prevent redundant game over calls
+            isGameOver = true;
+
+            gameOverText.SetActive(true);
         }
     }
