90843efbf4ee2fe9c7d28fe6ccd066fdff7efc8a
Roman Dulal <romandulal@Rowmahns-MacBook-Air.local>
2024-04-11 12:05:58 +0800

    Added design and functionality of Credit page

--- Assets/ARMenu/AR Menu Script/MenuManagerScript.cs
+++ Assets/ARMenu/AR Menu Script/MenuManagerScript.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuManagerScript : MonoBehaviour
 {
+    public GameObject creditPage;
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void CreditPageLoader()
+    {
+        creditPage.SetActive(true);
+    }
+
+    public void CreditPageExit()
+    {
+        creditPage.SetActive(false);
     }
 }
